430b65869ee5c70aa99d679b70bd76c56fbb1d43
Caleb Haynes <calebhaynes0@gmail.com>
2020-04-13 11:26:17 -0400

    adding comments and usage for fmriprep cleaning

--- clean_fmriPrep.py
+++ clean_fmriPrep.py
@@ -1,11 +1,23 @@
+#script for deleting unnecessary files from an fmriprep directory
+
+#usage: python3 clean_fmriPrep.py <path/to/fmriprep/directory/>
+
+#Caleb Haynes Spring 2020
+
 import os
 from os import path
 import glob
 import sys
 
 
-
 def cleaner(prepDir):
+'''
+this function tests to see whether the first argument is a directory (below
+it defaults to sys.argv[1] or the first argument you supply in the command
+line fter the name of the script). Then it uses glob (essentially regex) to 
+make a list to toRemove files. This is done piecemeal to allow for 'commenting 
+out' filetypes that you would like to maintain in your directory or other future customization
+'''
         if os.path.isdir(prepDir):
             print("Found directory {}".format(prepDir))
             MNI_fileList_anat = glob.glob("{}*/anat/*MNI152NLin6Asym*".format(prepDir))
